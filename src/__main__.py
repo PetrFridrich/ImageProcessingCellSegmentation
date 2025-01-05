252b2bf189093cfaa25fb6b96022359bd17519b0
PetrFridrich <petr.fridrich.1998@gmail.com>
2025-01-05 14:39:47 +0100

    just testing changes in segmenter class

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,26 @@
 import numpy as np
+from pathlib import Path
+import cv2
+import matplotlib.pyplot as plt
+import mahotas as mh
+from skimage import io
+
+from segmentation.ImageSegmentation import Segmenter
 
 def main():
+
+    IMAGE_PATH = Path(".", "data", "img_1.tif")
+    OUTPUT_PATH = Path(".", "results", IMAGE_PATH.stem, "IMG_flow")
+
+    img = io.imread(IMAGE_PATH)
+
+    img_segmenter = Segmenter(image=img)
+    
+    img_segmenter.apply_segmentation()
+    img_segmenter.save_image_flow(OUTPUT_PATH)
+
+
+
 
 
 
